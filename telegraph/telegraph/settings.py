bc5f29bbdddbcff53dd9b1cdf798de23fde5e32f
fasteidep <146346430+fasteidep@users.noreply.github.com>
2023-12-19 21:24:25 +0800

    Update settings.py

--- telegraph/telegraph/settings.py
+++ telegraph/telegraph/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--kq$r#!v9q^6r%eoxb1cfx=d@e-w8g%_i=96-9y#owf@^h1%w3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
